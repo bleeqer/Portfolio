30a28d5c10fc0d86456b3766ca46ccebf578b6de
bleeqer <bleeqtheproducer@gmail.com>
2022-02-13 17:55:51 +0900

    게시판 영속 계층 CRUD

--- src/test/java/com/portfolio/persistence/MyBatisTest.java
+++ src/test/java/com/portfolio/persistence/MyBatisTest.java
@@ -11,6 +11,9 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations={"file:web/WEB-INF/applicationContext.xml"})
 public class MyBatisTest {
@@ -29,8 +32,16 @@ public class MyBatisTest {
     }
 
     @Test
-    public void getBoard() {
-        BoardVO vo = boardMapper.getBoard();
+    public void testBoardMapper() {
+//        BoardVO selectedVO = boardMapper.select(1);
+//        createdVo.setTitle("인서트 테스트 제목");
+//        createdVo.setContent("인서트 테스트 컨텐트");
+//        createdVo.setWriter("작성");
+//        boardMapper.insert(createdVo);
+//        boardMapper.delete(2);
+//        selectedVO.setContent("he needs a queen!");
+//        boardMapper.update(selectedVO);
+//        List<BoardVO> vo = boardMapper.selectAll();
     }
 
 }
