23f995ce2ae6418e3553b922ac637d98092ed18b
bleeqer <bleeqtheproducer@gmail.com>
2022-02-13 19:54:06 +0900

    게시판 서비스 계층 작성

--- src/test/java/com/portfolio/persistence/MyBatisTest.java
+++ src/test/java/com/portfolio/persistence/MyBatisTest.java
@@ -2,6 +2,8 @@ package com.portfolio.persistence;
 
 import com.portfolio.domain.BoardVO;
 import com.portfolio.mapper.BoardMapper;
+import com.portfolio.service.BoardService;
+import com.portfolio.service.BoardServiceImpl;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -26,6 +28,9 @@ public class MyBatisTest {
     @Autowired
     BoardMapper boardMapper;
 
+    @Autowired
+    BoardService boardService;
+
     @Test
     public void testFactory() {
         System.out.println(sqlSessionFactory);
@@ -42,8 +47,29 @@ public class MyBatisTest {
 //        selectedVO.setContent("he needs a queen!");
 //        boardMapper.update(selectedVO);
 //        List<BoardVO> vo = boardMapper.selectAll();
-        boardMapper.addViewCnt(1);
-        List<BoardVO> vo = boardMapper.selectAll();
+//        boardMapper.addViewCnt(1);
+//        List<BoardVO> vo = boardMapper.selectAll();
     }
+
+    @Test
+    public void testBoardService() {
+        BoardVO createdVo = new BoardVO();
+        createdVo.setTitle("인서트 테스트 제목");
+        createdVo.setContent("인서트 테스트 컨텐트");
+        createdVo.setWriter("작성");
+
+        boardService.readAll();
+        boardService.read(1);
+        BoardVO selectedVO = boardMapper.select(1);
+        selectedVO.setContent("he needs a queen you know!");
+        boardService.create(createdVo);
+        boardService.update(selectedVO);
+        boardService.delete(3);
+        boardService.readAll();
+
+    }
+
+
+
 
 }
