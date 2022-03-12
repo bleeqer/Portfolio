a8dc6dba461d5296d39362347cee408678ee987a
bleeqer <bleeqtheproducer@gmail.com>
2022-03-13 05:12:01 +0900

    CustomAuthenticationSuccessHandler target, default 페이지 리디렉팅

--- src/main/java/com/portfolio/security/CustomAuthenticationProvider.java
+++ src/main/java/com/portfolio/security/CustomAuthenticationProvider.java
@@ -26,20 +26,13 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
     @Autowired
     PasswordEncoder passwordEncoder;
 
-    public CustomAuthenticationProvider() {
-        System.out.println("시발");
-    }
-
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        System.out.println("아니씨빨왜");
+
         String userId = (String) authentication.getPrincipal();
         String password = (String) authentication.getCredentials();
 
         CustomUserDetails user = (CustomUserDetails) userDetailsService.loadUserByUsername(userId);
-
-        System.out.println("Auth 유저네임: " + user.getUsername());
-        System.out.println("Auth 비밀번호: " + user.getPassword());
 
         if (!matchPassword(password, user.getPassword())) {
             throw new BadCredentialsException(userId);
@@ -47,8 +40,6 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
 
         List<GrantedAuthority> roles = new ArrayList<GrantedAuthority>();
         roles.add(new SimpleGrantedAuthority(user.getAuthority()));
-
-
 
         return new UsernamePasswordAuthenticationToken(userId, password, roles);
     }
@@ -60,7 +51,6 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
 
 
     private boolean matchPassword(String loginPwd, String password) {
-        System.out.println(passwordEncoder.matches(loginPwd, password));
         return passwordEncoder.matches(loginPwd, password);
     }
 }
--- src/main/java/com/portfolio/security/CustomAuthenticationSuccessHandler.java
+++ src/main/java/com/portfolio/security/CustomAuthenticationSuccessHandler.java
@@ -1,7 +1,14 @@
 package com.portfolio.security;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletException;
@@ -11,8 +18,38 @@ import java.io.IOException;
 
 @Component
 public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+    private String defaultUrl;
+
+    private RequestCache requestCache = new HttpSessionRequestCache();
+    private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
-        System.out.println("로그인 성공");
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+
+        resultRedirectStrategy(request, response, authentication);
+
+    }
+
+    private void resultRedirectStrategy(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+
+        SavedRequest savedRequest = requestCache.getRequest(request, response);
+
+        if (savedRequest!=null) {
+            String targetUrl = savedRequest.getRedirectUrl();
+            redirectStrategy.sendRedirect(request, response, targetUrl);
+        } else {
+            redirectStrategy.sendRedirect(request, response, defaultUrl);
+        }
+    }
+
+
+
+    public String getDefaultUrl() {
+        return defaultUrl;
+    }
+
+    public void setDefaultUrl(String defaultUrl) {
+        this.defaultUrl = defaultUrl;
     }
 }
