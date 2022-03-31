f5f72e105e531e82a78b5d2b8c7ac93e408629b9
bleeqer <bleeqtheproducer@gmail.com>
2022-03-31 19:02:07 +0900

    QuestionCategoryService, Impl 작성

---
+++ src/main/java/com/portfolio/service/QuestionCategoryService.java
@@ -0,0 +1,11 @@
+package com.portfolio.service;
+
+import com.portfolio.domain.AnswerReplyVO;
+import com.portfolio.domain.QuestionCategoryVO;
+
+import java.util.HashMap;
+import java.util.List;
+
+public interface QuestionCategoryService {
+    List<QuestionCategoryVO> selectAll();
+}
---
+++ src/main/java/com/portfolio/service/QuestionCategoryServiceImpl.java
@@ -0,0 +1,26 @@
+package com.portfolio.service;
+
+import com.portfolio.domain.AnswerReplyVO;
+import com.portfolio.domain.QuestionCategoryVO;
+import com.portfolio.mapper.AnswerReplyMapper;
+import com.portfolio.mapper.QuestionCategoryMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashMap;
+import java.util.List;
+
+
+@Service
+public class QuestionCategoryServiceImpl implements QuestionCategoryService {
+
+    @Autowired
+    QuestionCategoryMapper questionCategoryMapper;
+
+
+    @Override
+    public List<QuestionCategoryVO> selectAll() {
+        return questionCategoryMapper.selectAll();
+    }
+}
